18bffe86aa195b600c3b72f72d92341e62d3036e
Apurv <apoorvagarwal00@gmail.com>
2025-03-24 23:47:13 -0700

    order of ids

--- app.py
+++ app.py
@@ -851,6 +851,8 @@ def serve_reviews1(current_product, rate, page):
 
 
     filtered_reviews = [review for review in review_data if review.get('Number') == rate]
+
+
     # random_order_required = random_order[:len(filtered_reviews)]
 
     ## Calculate the length of the filtered reviews, then keep only the numbers that fall in range(length of the filtered reviews)
@@ -863,12 +865,16 @@ def serve_reviews1(current_product, rate, page):
 
     number_of_reviews_per_page = product_data.get('number_of_reviews_per_page')
 
-    randomized_filtered_reviews = randomized_filtered_reviews[(page-1)*number_of_reviews_per_page: min(page*number_of_reviews_per_page, len(filtered_reviews))]
+    order_of_all_ids = '-'.join([str(review['id']) for review in randomized_filtered_reviews])
+
+    randomized_filtered_reviews_truncated = randomized_filtered_reviews[(page-1)*number_of_reviews_per_page: min(page*number_of_reviews_per_page, len(filtered_reviews))]
     list_of_review_ids = random_order_required[(page-1)*number_of_reviews_per_page: min(page*number_of_reviews_per_page, len(filtered_reviews))]
 
-    print(randomized_filtered_reviews)
-    order_of_ids = '-'.join([str(review['id']) for review in randomized_filtered_reviews])
-    return render_template('r32.html', current_product=current_product, lab_id=lab_id, review_clicked_records=product_data["review_read_data_all"], reviews=randomized_filtered_reviews, rate=rate, page=page, product=product_data, number_of_pages=number_of_reviews_per_page*len(filtered_reviews), list_of_review_ids = ','.join([str(r) for r in list_of_review_ids]), order_of_ids=order_of_ids)
+    print(randomized_filtered_reviews_truncated)
+    order_of_ids = '-'.join([str(review['id']) for review in randomized_filtered_reviews_truncated])
+
+
+    return render_template('r32.html', current_product=current_product, lab_id=lab_id, review_clicked_records=product_data["review_read_data_all"], reviews=randomized_filtered_reviews, rate=rate, page=page, product=product_data, number_of_pages=number_of_reviews_per_page*len(filtered_reviews), list_of_review_ids = ','.join([str(r) for r in list_of_review_ids]), order_of_ids=order_of_all_ids)
 
 
 @app.route('/t/<string:current_product>/0/<int:page>')
